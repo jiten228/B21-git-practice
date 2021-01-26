775c78d3d00d0177bc938b55de4a691956f029e2
jiten228 <jiten228@yahoo.com>
2021-01-26 14:23:35 -0600

    movie 1 updated again Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World , B21");
-        System.out.println("B21 future SDET");
+        System.out.println("B21 future SDET learning Git");
     }
 }
